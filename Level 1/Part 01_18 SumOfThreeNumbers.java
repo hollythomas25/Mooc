ce63a2ee236230d5bdeed675d0c08467bfd82410
Holly Thomas <110477818+hollythomas25@users.noreply.github.com>
2023-04-25 16:50:59 -0500

    Final

---
+++ Level 1/Part 01_18 SumOfThreeNumbers.java
@@ -0,0 +1,22 @@
+ 
+	import java.util.Scanner;
+	 
+	public class SumOfThreeNumbers {
+	 
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	 
+	        System.out.println("Give the first number:");
+	        int number1 = Integer.valueOf(scanner.nextLine());
+	        
+	        System.out.println("Give the second number:");
+	        int number2 = Integer.valueOf(scanner.nextLine());
+	        
+	        System.out.println("Give the third number:");
+	        int number3 = Integer.valueOf(scanner.nextLine());
+	        
+	        int sum = number1 + number2 + number3;
+	        
+	        System.out.println("The sum of the numbers is " + sum);
+	    }
+	}
