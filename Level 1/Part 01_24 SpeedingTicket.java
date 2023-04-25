ce10cf653f5aa05d8e1989b017d1b7836429acf4
Holly Thomas <110477818+hollythomas25@users.noreply.github.com>
2023-04-25 16:58:13 -0500

    Final

---
+++ Level 1/Part 01_24 SpeedingTicket.java
@@ -0,0 +1,28 @@
+ 
+	import java.util.Scanner;
+	 
+	public class SimpleCalculator {
+	 
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	 
+	        System.out.println("Give the first number:");
+	        int number1 = Integer.valueOf(scanner.nextLine());
+	        
+	        System.out.println("Give the second number:");
+	        int number2 = Integer.valueOf(scanner.nextLine());
+	        
+	        int sum = number1 + number2;
+	        System.out.println(number1 + " + " + number2 + " = " + sum);
+	        
+	        int diff = Math.abs(number1 - number2);
+	        System.out.println(number1 + " - " + number2 + " = " + diff);
+	        
+	        int multiply = number1 * number2;
+	        System.out.println(number1 + " * " + number2 + " = " +  multiply);
+	        
+	        double division = (double) number1 / number2;
+	        System.out.println(number1 + " / " + number2 + " = " + division);
+	     
+	    }
+	}
